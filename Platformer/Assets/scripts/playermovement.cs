8ecfe4a6a7e657a40e6f8544e1198aba05ef7fed
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-05-03 13:08:42 +0530

    Added some playermovement left/right

---
+++ Platformer/Assets/scripts/playermovement.cs
@@ -0,0 +1,21 @@
+
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y);
+    }
+
+
+}
