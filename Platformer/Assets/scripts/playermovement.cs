a5bf3868b010466917e7d1a2d88d5444b1c3bd9e
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-05-04 12:40:19 +0530

    added the basic player animations

--- Platformer/Assets/scripts/playermovement.cs
+++ Platformer/Assets/scripts/playermovement.cs
@@ -5,12 +5,13 @@ public class playermovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
-
+    private Animator anim;
 
     private void Awake()
     {
+        //gets references for rigidbody and animator grom object
         body = GetComponent<Rigidbody2D>();
-
+        anim = GetComponent<Animator>();
     }
 
 
@@ -25,13 +26,19 @@ public class playermovement : MonoBehaviour
         if (horizontalInput > 0.01f)
             transform.localScale = Vector3.one;
         else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-1, 1, 1); 
+            transform.localScale = new Vector3(-1, 1, 1);
 
 
         if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);
-    }
 
+        //set animator parameters
+        anim.SetBool("run", horizontalInput != 0);
+
+
+
+
+    }
 
 
 }
