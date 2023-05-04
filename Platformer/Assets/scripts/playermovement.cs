db2f005d1b1b2174bbeb28aa12940c5598d33aa5
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-05-04 16:12:14 +0530

    Jump machanic

--- Platformer/Assets/scripts/playermovement.cs
+++ Platformer/Assets/scripts/playermovement.cs
@@ -6,7 +6,7 @@ public class playermovement : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D body;
     private Animator anim;
-
+    private bool grounded;
     private void Awake()
     {
         //gets references for rigidbody and animator grom object
@@ -29,16 +29,33 @@ public class playermovement : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
 
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKey(KeyCode.Space)&& grounded)
+            Jump();
 
         //set animator parameters
         anim.SetBool("run", horizontalInput != 0);
-
+        anim.SetBool("grounded", grounded);
 
 
 
     }
 
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("Jump");
+        grounded = false;
+   }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+
+
+
+
 
 }
+
