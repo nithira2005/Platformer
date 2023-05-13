56819c5cd56bc0169f4cc32946f4a11e7f0c6947
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-05-13 14:06:00 +0530

    Player Camera

--- Platformer/Assets/scripts/CameraController.cs
+++ Platformer/Assets/scripts/CameraController.cs
@@ -3,14 +3,24 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    //Room camera
     [SerializeField] private float speed;
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
 
+    //Follow Player
+    [SerializeField]private Transform player;
+
+
     private void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+        //Room camera
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+
+
     }
+
     public void MovetoNewRoom(Transform _newroom)
     {
         currentPosX = _newroom.position.x;
