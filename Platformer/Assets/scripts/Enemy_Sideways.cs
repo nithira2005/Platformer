f827bc8962567174ce930fc5766d9b25b87b0b5a
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-05-21 13:56:16 +0530

    Added a Saw that can hurt the player

---
+++ Platformer/Assets/scripts/Enemy_Sideways.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Enemy_Sideways : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<Health>().takeDamage(damage);
+        }
+    }
+
+
+
+
+
+}
