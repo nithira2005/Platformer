d515a5e8cd2da9dfff76d59d04436f3a6bf43172
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-05-27 14:08:55 +0530

    Bug fixed iFrames are working as intended

--- Platformer/Assets/scripts/Health/Health.cs
+++ Platformer/Assets/scripts/Health/Health.cs
@@ -1,32 +1,38 @@
 using UnityEngine;
+using System.Collections;
 
 public class Health : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
-
     public void TakeDamage(float _damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
         if (currentHealth > 0)
         {
-            //player hurt
             anim.SetTrigger("hurt");
-            //iframes
+            StartCoroutine(Invunerability());
         }
         else
         {
             if (!dead)
             {
-                //player dead
                 anim.SetTrigger("die");
                 GetComponent<playermovement>().enabled = false;
                 dead = true;
@@ -37,7 +43,16 @@ public class Health : MonoBehaviour
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
-
-
-
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+    }
 }
