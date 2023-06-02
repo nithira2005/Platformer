c9c88c035cc8defcac2219537853d24d3df709b2
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-06-02 14:23:40 +0530

    Arrow trap tweaks

--- Platformer/Assets/scripts/Enemy/EnemyProjectile.cs
+++ Platformer/Assets/scripts/Enemy/EnemyProjectile.cs
@@ -2,9 +2,27 @@ using UnityEngine;
 
 public class EnemyProjectile : Enemydamage
 {
-    public void SetDirection(float _direction)
+    [SerializeField] private float speed;
+    [SerializeField] private float resetTime;
+    private float lifetime;
+    public void ActivateProjectile()
     {
+        lifetime = 0;
+        gameObject.SetActive(false);
 
     }
+    private void Update()
+    {
+        float movementSpeed = speed * Time.deltaTime;
+        transform.Translate(movementSpeed, 0, 0);
 
+        lifetime += Time.deltaTime;
+        if (lifetime > resetTime)
+            gameObject.SetActive(false);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
 }
