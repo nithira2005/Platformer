4379ae9227788dc330676d75b7846bc741538173
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-06-09 14:18:10 +0530

    Added a collider to detect player

--- Platformer/Assets/scripts/Enemy/lilbro.cs
+++ Platformer/Assets/scripts/Enemy/lilbro.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class lilbro : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private int damage;
+    [SerializeField]private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        cooldownTimer += Time.deltaTime;
+        //attackwhen player on range
+        if (PlayerInsight())
+        {
+            if (cooldownTimer >= attackCooldown)
+            {
+                //attack
+            }
+        }
+    }
+     private bool PlayerInsight()
+     {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.left, 0, playerLayer);
+
+        return hit.collider != null;
+     }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center, boxCollider.bounds.size);
     }
 }
