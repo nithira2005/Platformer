f5ce8c8fd6d5d8d6f4ce34a912820958fb7bd013
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-06-09 17:30:57 +0530

    enemy can damage now

--- Platformer/Assets/scripts/Enemy/lilbro.cs
+++ Platformer/Assets/scripts/Enemy/lilbro.cs
@@ -11,6 +11,7 @@ public class lilbro : MonoBehaviour
     private float cooldownTimer = Mathf.Infinity;
 
     private Animator anim;
+    private Health playerHealth; 
 
     private void Awake()
     {
@@ -33,7 +34,10 @@ public class lilbro : MonoBehaviour
      private bool PlayerInsight()
      {
         RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range*transform.localScale.x* colliderDistance, new Vector3(boxCollider.bounds.size.x*range,boxCollider.bounds.size.y,boxCollider.bounds.size.z), 0, Vector2.left, 0, playerLayer);
-        
+
+        if (hit.collider != null)
+            playerHealth = hit.transform.GetComponent<Health>();
+       
         return hit.collider != null;
      }
     private void OnDrawGizmos()
@@ -41,6 +45,12 @@ public class lilbro : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x*colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z)); 
     }
-
+ private void DamagePlayer()
+    {
+        //damages player is he on range
+        if (PlayerInsight())
+         playerHealth.TakeDamage(damage);
+        
+    }
 }
  
