b38980bdac62b688f50ad352cc67635e83a8be75
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-06-09 14:55:18 +0530

    Allow to control the collider distance

--- Platformer/Assets/scripts/Enemy/lilbro.cs
+++ Platformer/Assets/scripts/Enemy/lilbro.cs
@@ -4,6 +4,7 @@ public class lilbro : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
     [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
     [SerializeField] private int damage;
     [SerializeField]private BoxCollider2D boxCollider;
     [SerializeField] private LayerMask playerLayer;
@@ -23,13 +24,14 @@ public class lilbro : MonoBehaviour
     }
      private bool PlayerInsight()
      {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range, boxCollider.bounds.size, 0, Vector2.left, 0, playerLayer);
-
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range*transform.localScale.x* colliderDistance, new Vector3(boxCollider.bounds.size.x*range,boxCollider.bounds.size.y,boxCollider.bounds.size.z), 0, Vector2.left, 0, playerLayer);
+        
         return hit.collider != null;
      }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(boxCollider.bounds.center+transform.right*range, boxCollider.bounds.size);
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x*colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z)); 
     }
 }
+ 
