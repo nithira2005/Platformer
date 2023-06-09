18ca2e6cb00e7e0f8e0eb3df89896df1037138c4
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-06-09 15:26:18 +0530

    Save file in unity

--- Platformer/Assets/scripts/Enemy/lilbro.cs
+++ Platformer/Assets/scripts/Enemy/lilbro.cs
@@ -41,5 +41,6 @@ public class lilbro : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x*colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z)); 
     }
+
 }
  
