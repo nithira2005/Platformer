932c6a9b30cc26ff4338d663f0ffe5720d67b1f5
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-06-10 11:09:11 +0530

    Enemy movement

--- Platformer/Assets/scripts/Enemy/enemyPatrol.cs
+++ Platformer/Assets/scripts/Enemy/enemyPatrol.cs
@@ -11,8 +11,17 @@ public class enemyPatrol : MonoBehaviour
 
     [Header("Movement perameters")]
     [SerializeField] private float speed;
+    private Vector3 initScale;
+
+    private void Awake()
+    {
+        initScale = enemy.localScale;
+    }
     private void MoveInDirection(int _direction)
     {
+        //face right direction
+        enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction, initScale.y, initScale.z);
+        //moving direction
         enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed, enemy.position.y, enemy.position.z);
     }
 }
