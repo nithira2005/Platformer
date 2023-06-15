284d7a740766fd9914d6818cde46d0b4f961f966
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-06-15 18:31:12 +0530

    Interactions on UI

--- Platformer/Assets/scripts/UI/selectionArrow.cs
+++ Platformer/Assets/scripts/UI/selectionArrow.cs
@@ -3,10 +3,40 @@ using UnityEngine;
 public class selectionArrow : MonoBehaviour
 {
    [SerializeField] private RectTransform[] options;
+    [SerializeField] private AudioClip changeSound;
+    [SerializeField] private AudioClip interactSound;
     private RectTransform rect;
+    private int currentPostition;
 
     private void Awake()
     {
         rect = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            ChangePosition(-1);
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            ChangePosition(1);
+    }
+
+
+    private void ChangePosition(int _change)
+    {
+        currentPostition += _change;
+        if (_change != 0)
+            soundManager.instance.PlaySound(changeSound);
+
+
+
+        if (currentPostition < 0)
+            currentPostition = options.Length - 1;
+        else if (currentPostition > options.Length - 1)
+            currentPostition = 0;
+
+        
+        //assign the y pos yo current pos to arrow
+        rect.position = new Vector3(rect.position.x, options[currentPostition].position.y, 0);
     }
 }
