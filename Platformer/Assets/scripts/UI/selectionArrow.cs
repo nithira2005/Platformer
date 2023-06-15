8b86829ab83a1661e8ef06d8155d697ebdf29632
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-06-15 18:05:58 +0530

    UI improvements

--- Platformer/Assets/scripts/UI/selectionArrow.cs
+++ Platformer/Assets/scripts/UI/selectionArrow.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class selectionArrow : MonoBehaviour
 {
+   [SerializeField] private RectTransform[] options;
     private RectTransform rect;
 
     private void Awake()
