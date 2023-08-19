578ee41a5c67a218f2f1741722d30d3305decb1c
Nithira Rajapaksha <92735644+nithira2005@users.noreply.github.com>
2023-08-19 09:48:04 +0530

    Changed the Camera

--- Platformer/Assets/scripts/core/CameraController.cs
+++ Platformer/Assets/scripts/core/CameraController.cs
@@ -9,16 +9,16 @@ public class CameraController : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
 
     //Follow Player
-    //[SerializeField] private Transform player;
-    //[SerializeField] private float aheadDistance;
-   // [SerializeField] private float cameraSpeed;
-    //private float lookAhead;
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
     private void Update()
     {
         //Room camera
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
-        //transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
-        //lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+       // transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
 
     }
 
